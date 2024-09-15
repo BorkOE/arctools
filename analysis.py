8944011df9c75345041e2f90bd37ee3694184921
BorkOE <oskar__larsson94@hotmail.com>
2024-09-15 15:36:48 +0200

    new and changed functionality

--- analysis.py
+++ analysis.py
@@ -7,13 +7,15 @@ from . import helpers
 
 cwd = os.getcwd()
 
-def calculate_geometry(in_features, geometry_property, length_unit='METERS', **kwargs):
+def calculate_geometry(in_features, geometry_property, **kwargs):
     '''
     in features: str, eg. "Roads_within"
-    geometry_property: str, eg. "LENGTH LENGTH"'''
+    geometry_property: str, eg. "LENGTH LENGTH", "Area_field AREA_GEODESIC"
+    length_unit: str, eg. "METERS"
+    area_unit: str, eg. "SQUARE_KILOMETERS", "HECTARES"
+    '''
     arcpy.management.CalculateGeometryAttributes(in_features, 
                                                  geometry_property, 
-                                                 length_unit,
                                                  **kwargs)
 
 def overlay_intersect(in_features, out_path, out_name, **kwargs):
@@ -27,3 +29,10 @@ def overlay_intersect(in_features, out_path, out_name, **kwargs):
                                 f"{out_path}\{out_name}", 
                                 **kwargs
                                 )
+
+def coordinates_from_point_geometry(frame, new_x_col='x', new_y_col='y', shape_col='SHAPE'):
+    '''returns frame with new coordinate cols
+    TODO: expect it to work with polygons also, check
+    '''
+    frame[new_x_col], frame[new_y_col] = zip(*frame[shape_col].apply(lambda x: (x.centroid[0], x.centroid[1])))
+    return frame
--- basic.py
+++ basic.py
@@ -5,6 +5,22 @@ TODO:
 
     - lyr.visible = False
         - Updates in map view. Can build functionality around this consept. lyr is layer object
+
+    - Export spatial dataframe 
+        - sdf.spatial.to_featureclass(location=r"c:\output_examples\census.shp")
+
+    - delete fields (new file: data?)
+        arcpy.management.DeleteField(lyr, sdf.loc[:,'CEEAC':'LDC'].columns.to_list())
+
+    - Add fields
+        arcpy.management.AddField(
+        lyr,
+        'Area',
+        'FLOAT',
+        )
+
+    - Other interesting:
+        - map.clearSelection() # Clears the selection for all layers and tables in a map
 '''
 
 from arcgis.features import GeoAccessor, GeoSeriesAccessor
@@ -13,6 +29,7 @@ import pandas as pd
 import arcpy
 import os
 from . import helpers
+import tempfile
 
 cwd = os.getcwd()
 # Keep as globals to simplyfy workflow, change with relevant set function
@@ -26,21 +43,30 @@ def get_currrent_project():
 def get_map(**kwargs):
     if not kwargs.get('map_name'):   # Fetch by map_index, def. first project map
         map_name = aprx.listMaps()[kwargs.get('map_index', 0)].name
+    else:
+        map_name = kwargs.get('map_name')
 
     print(f'fetching map: {map_name}')
     return aprx.listMaps(map_name)[0]
 
-def get_layer(**kwargs):
+def get_layer(supress_print=False, **kwargs):
+    '''returns layer object'''
     if not map:
-        print('Need to set map first!')
-        return
+        set_map()
     if not kwargs.get('layer_name'):
         layer_name = map.listLayers()[kwargs.get('layer_index', 0)].name    # def. first layer
     else:
         layer_name = kwargs.get('layer_name')
     
-    print(f'fetching layer: {layer_name}')
+    if not supress_print:
+        print(f'fetching layer: {layer_name}')
     return map.listLayers(layer_name)[0]
+
+def get_layer_names(**kwargs):
+    '''returns list of layers in map'''
+    if not map:
+        set_map()
+    return [l.name for l in map.listLayers()]
 
 def set_map(**kwargs):
     '''
@@ -49,9 +75,11 @@ def set_map(**kwargs):
     global map
     map = get_map(**kwargs)
     print(f'map set: {map.name}')
+    return map
 
 def set_layer(**kwargs):
     '''
+    sets layer in module memory, also returns it
     layer_name: str
     layer_index: int, def. 0'''
     global lyr, map
@@ -59,18 +87,74 @@ def set_layer(**kwargs):
         set_map()
     lyr = get_layer(**kwargs)
     print(f'layer set: {lyr.name}')
+    return lyr
 
-def get_layer_dataframe(clear_select=True, **kwargs):
-    '''Fetches dataframe
-    clear_selection: bool, it False, only return dataframe for selected features in layer attribute table. Else clears selection.
+def set_visible(show_layers, hide_all_other=True, **kwargs):
     '''
+    show_layers: list of layer names to be set as visible'''
+    all_layers = set(get_layer_names(**kwargs))
+    show_layers = set(show_layers)
+
+    # Find and print faulty layer input
+    not_recognized = show_layers.difference(all_layers)
+    if not_recognized:
+        print(f'Did not recognize layers: {not_recognized}')
+    
+    # Only make visible layers that exist
+    show_layers = show_layers.intersection(all_layers)
+
+    # Find layers to hide
+    hide_layers = all_layers.difference(show_layers)
+
+    # print('Show:')
+    # print(show_layers)
+    # print('Hide:')
+    # print(hide_layers)
+
+    if hide_all_other:
+        for l in hide_layers:
+            lyr_temp = get_layer(supress_print=True, layer_name=l)
+            lyr_temp.visible = False
+
+    for l in show_layers:
+        lyr_temp = get_layer(supress_print=True, layer_name=l)
+        lyr_temp.visible = True
+
+def sdf_to_df(sdf_frame, **kwargs):
+    '''writes/reads tempfile in cwd
+    shapecol_to_drop: str, def. SHAPE
+    
+    Note encoding='latin-1' in pd.read_csv to handle scandinavian letters'''
+    path = os.getcwd() + r'\temp'
+    os.makedirs(path, exist_ok=True)
+    f = tempfile.NamedTemporaryFile(dir=path)
+    with open(f.name + '.csv', 'w') as file:
+        (sdf_frame
+            .drop(columns=kwargs.get('shapecol_to_drop', 'SHAPE'))
+            .to_csv(file , sep=';', encoding='UTF-8'))
+    frame = pd.read_csv(f.name + '.csv', sep=';', encoding='latin-1', index_col=0)
+    f.close()   # Close tempfile
+    return frame
+
+def get_layer_dataframe(clear_select=True, sdf=False, **kwargs):
+    '''Fetches dataframe
+    layer_name: str
+    layer_idx: int
+    clear_selection: bool, it False, only return dataframe for selected features in layer attribute table. Else clears selection.
+    sdf: bool, returns spatially enabled dataframe instead of regular pandas dataframe
+    '''
+    set_layer(**kwargs)
     if not lyr:
         print('Needs to set layer first!')
         return
     if clear_select:
         clear_selection(layer=lyr)   # Clears possible selection, else incomplete dataframe is returned
-    sdf = pd.DataFrame.spatial.from_featureclass(lyr)
-    return sdf
+    out_frame = pd.DataFrame.spatial.from_featureclass(lyr)
+    if sdf:
+        return out_frame
+    else:
+        return sdf_to_df(out_frame)
+        # return pd.read_clipboard(out_frame.drop(columns='SHAPE').to_clipboard())
 
 def load_data(paths, **kwargs):
     '''Loads data from path, tested with shapefile. If no map specified, fetches first map
@@ -133,11 +217,12 @@ def clear_selection(how='', layer=''):
         if not map:
             print('load a map first using set_map')
             return
-        for lyr in [l.name for l in map.listLayers()]:
-            try:
-                arcpy.SelectLayerByAttribute_management(lyr, "CLEAR_SELECTION")
-            except Exception as e:
-                print(f'Exception occured while clearing selection for layer: {lyr}')
+        map.clearSelection()
+        # for lyr in [l.name for l in map.listLayers()]:
+        #     try:
+        #         arcpy.SelectLayerByAttribute_management(lyr, "CLEAR_SELECTION")
+        #     except Exception as e:
+        #         print(f'Exception occured while clearing selection for layer: {lyr}')
     elif layer:
         arcpy.SelectLayerByAttribute_management(helpers.get_layer_name(layer), "CLEAR_SELECTION")    
     else:
